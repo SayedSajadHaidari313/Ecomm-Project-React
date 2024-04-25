f23ae613fb4a2840af88c797f1568b1e58bf1426
Sayed Sajad Haidari <sayedsajadhaidari1@gmail.com>
2024-04-25 17:49:27 +0430

    new changes in product list

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,13 +1,59 @@
-import React from 'react'
-import Header from './Header'
-function ProductList() {
-  return (
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+import { Table } from 'react-bootstrap';
 
+function ProductList() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/api/list');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const result = await response.json();
+        setProducts(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData(); // Call the async function
+  }, []);
+
+  return (
     <div>
-      <Header/>
-      <h1>ProductList</h1>
+      <Header />
+      <h1 className="text-center mb-4">Product List</h1>
+      <div className='container'>
+      <Table striped bordered hover>
+        <thead className="thead-dark">
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Description</th>
+            <th>Image</th>
+          </tr>
+        </thead>
+        <tbody>
+        {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.id}</td>
+              <td>{product.name}</td>
+              <td>${product.price}</td>
+              <td>{product.description}</td>
+              <td>
+              <img style={{width:100}} src={"http://localhost:8000/" + product.file_path} alt={product.name} className="img-fluid" />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProductList
+export default ProductList;
