160218b5a9236c7ebc073afdf951b0bf4c50cc77
Sayed Sajad Haidari <sayedsajadhaidari1@gmail.com>
2024-04-30 06:42:15 +0430

    product lis

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import Header from './Header';
-import { Table } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+import { Table } from "react-bootstrap";
 
 function ProductList() {
   const [products, setProducts] = useState([]);
@@ -8,14 +8,14 @@ function ProductList() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:8000/api/list');
+        const response = await fetch("http://localhost:8000/api/list");
         if (!response.ok) {
-          throw new Error('Failed to fetch data');
+          throw new Error("Failed to fetch data");
         }
         const result = await response.json();
         setProducts(result);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -26,31 +26,36 @@ function ProductList() {
     <div>
       <Header />
       <h1 className="text-center mb-4">Product List</h1>
-      <div className='container'>
-      <Table striped bordered hover>
-        <thead className="thead-dark">
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Description</th>
-            <th>Image</th>
-          </tr>
-        </thead>
-        <tbody>
-        {products.map((product) => (
-            <tr key={product.id}>
-              <td>{product.id}</td>
-              <td>{product.name}</td>
-              <td>${product.price}</td>
-              <td>{product.description}</td>
-              <td>
-              <img style={{width:100}} src={"http://localhost:8000/" + product.file_path} alt={product.name} className="img-fluid" />
-              </td>
+      <div className="container">
+        <Table striped bordered hover>
+          <thead className="thead-dark">
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Description</th>
+              <th>Image</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id}>
+                <td>{product.id}</td>
+                <td>{product.name}</td>
+                <td>${product.price}</td>
+                <td>{product.description}</td>
+                <td>
+                  <img
+                    style={{ width: 100 }}
+                    src={"http://localhost:8000/" + product.file_path}
+                    alt={product.name}
+                    className="img-fluid"
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
     </div>
   );
